c9fd0be81eb83f60563548fdd29c09a48bbd6cb6
Alex Godfrey <alexjamesgodfrey@gmail.com>
2020-12-06 20:36:55 -0500

    added animation to change cards

--- js/index.js
+++ js/index.js
@@ -47,6 +47,7 @@ function cardTrick(target, duration) {
       translateY: '-10%',
       rotate: 10
     });
+    //at this point the target z-index will be set to -10 by the cardZVar function
     tl
     .add({
       translateX: 0,
@@ -67,6 +68,7 @@ function cardTrick(target, duration) {
       translateY: '-10%',
       rotate: 10
     });
+    //at this point the target z-index will be set to 10 by the cardZVar function
     tl
     .add({
       translateX: 0,
@@ -80,27 +82,69 @@ const piggyTrick = () => cardTrick(piggyPng, 400);
 piggyPng.addEventListener('click', piggyTrick);
 piggyDiv.addEventListener('click', piggyTrick);
 
+
+
+
 let aboutDiv = document.getElementById('about-me');
 
 let prevArrow = document.getElementById('previous');
 let shuffle = document.getElementById('shuffle');
 let nextArrow = document.getElementById('next');
 let pictureList = [aboutDiv, piggyDiv];
+let last = piggyDiv;
+
+function cardSwapTrick(target,duration, zIndex) {
+  let tl = anime.timeline({
+    targets: target,
+    duration: duration,
+    easing: 'easeOutQuint'
+  });
+  tl
+  .add({
+    translateX: '100%',
+    translateY: '-10%',
+    rotate: 10
+  });
+  //this is when the z-index of the current card should be negated
+  tl
+  .add({
+    translateX: 0,
+    translateY: 0,
+    rotate: 0
+  })
+  if (target === last){
+    const changeZAll = () => zReturn();
+    setTimeout(changeZAll, duration);
+  }else {
+    const changeZ = () => cardZ(target, 9);
+    setTimeout(changeZ, duration);
+  }
+}
+
+function zReturn(){
+  for (let i=0; i<pictureList.length; i++){
+    pictureList[i].style.zIndex = '10';
+  }
+}
 
 let current = 0;
-function cardSwap(button){
+function cardSwapFull(button){
   let length = pictureList.length;
+
+  //next arrow click
   if (button === nextArrow){
-    pictureList[current].style.display = 'none';
+    if (current !== length-1){
+      cardSwapTrick(pictureList[current], 400);
+    }else {
+      cardSwapTrick(pictureList[current], 400);
+    }
     if (current === length-1){
       current = 0;
     }else {
       current++;
     }
-    console.log(current);
-    pictureList[current].style.display = 'flex';
   }
 }
 
-const next = () => cardSwap(nextArrow);
+const next = () => cardSwapFull(nextArrow);
 nextArrow.addEventListener('click', next);
