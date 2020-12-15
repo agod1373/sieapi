71ed9efd0b3a16f9ce27b1c80239a737c2bd777e
Alex Godfrey <alexjamesgodfrey@gmail.com>
2020-12-14 19:25:57 -0500

    updated random function

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ class Piece {
     }
   }
 
+
   /*
   changeZ(){
     target.style.zIndex = z;
@@ -79,7 +80,6 @@ const blanket = new Piece('blanket', 'blanket-png');
 const ceiling = new Piece('ceiling', 'ceiling-png');
 const sabetha = new Piece('sabetha', 'sabetha-png');
 const icicles = new Piece('icicles', 'icicles-png');
-const alex = new Piece('alex', 0);
 
 
 
@@ -135,42 +135,37 @@ function cardTrick(target, duration) {
 }
 
 
-const elephantTrick = () => cardTrick(elephant.png, 400);
-elephant.div.addEventListener('click', elephantTrick);
-const naplesTrick = () => cardTrick(naples.png, 400);
-naples.div.addEventListener('click', naplesTrick);
-const bricksTrick = () => cardTrick(bricks.png, 400);
-bricks.div.addEventListener('click', bricksTrick);
-const turtleTrick = () => cardTrick(turtle.png, 400);
-turtle.div.addEventListener('click', turtleTrick);
-const creamTrick = () => cardTrick(cream.png, 400);
-cream.div.addEventListener('click', creamTrick);
-const bottleTrick = () => cardTrick(bottle.png, 400);
-bottle.div.addEventListener('click', bottleTrick);
-const piggyTrick = () => cardTrick(piggy.png, 400);
-piggy.div.addEventListener('click', piggyTrick);
-const guitarTrick = () => cardTrick(guitar.png, 400);
-guitar.div.addEventListener('click', guitarTrick);
-const jasmineTrick = () => cardTrick(jasmine.png, 400);
-jasmine.div.addEventListener('click', jasmineTrick);
-const blanketTrick = () => cardTrick(blanket.png, 400);
-blanket.div.addEventListener('click', blanketTrick);
-const ceilingTrick = () => cardTrick(ceiling.png, 400);
-ceiling.div.addEventListener('click', ceilingTrick);
-const sabethaTrick = () => cardTrick(sabetha.png, 400);
-sabetha.div.addEventListener('click', sabethaTrick);
-const iciclesTrick = () => cardTrick(icicles.png, 400);
-icicles.div.addEventListener('click', iciclesTrick);
+
+
 
 
 let prevArrow = document.getElementById('previous');
 let shuffle = document.getElementById('shuffle');
 let nextArrow = document.getElementById('next');
-let pictureList = [sierra.div, elephant.div, naples.div, bricks.div, turtle.div, cream.div, bottle.div, piggy.div, guitar.div, jasmine.div, blanket.div, ceiling.div, sabetha.div, icicles.div, alex.div];
-let pngList = [sierra.div, elephant.png, naples.png, bricks.png, turtle.png, cream.png, bottle.png, piggy.png, guitar.png, jasmine.png, blanket.png, ceiling.png, sabetha.png, icicles.png, alex.div];
-let length = pictureList.length;
-let first = pictureList[0];
-let last = pictureList[length - 1];
+let divList = [sierra.div, elephant.div, naples.div, bricks.div, turtle.div, cream.div, bottle.div, piggy.div, guitar.div, jasmine.div, blanket.div, ceiling.div, sabetha.div, icicles.div];
+let pngList = [sierra.div, elephant.png, naples.png, bricks.png, turtle.png, cream.png, bottle.png, piggy.png, guitar.png, jasmine.png, blanket.png, ceiling.png, sabetha.png, icicles.png];
+
+let length = divList.length;
+let first = divList[0];
+let last = divList[length - 1];
+
+const elephantTrick = () => cardTrick(elephant.png, 400);
+const naplesTrick = () => cardTrick(naples.png, 400);
+const bricksTrick = () => cardTrick(bricks.png, 400);
+const turtleTrick = () => cardTrick(turtle.png, 400);
+const creamTrick = () => cardTrick(cream.png, 400);
+const bottleTrick = () => cardTrick(bottle.png, 400);
+const piggyTrick = () => cardTrick(piggy.png, 400);
+const guitarTrick = () => cardTrick(guitar.png, 400);
+const jasmineTrick = () => cardTrick(jasmine.png, 400);
+const blanketTrick = () => cardTrick(blanket.png, 400);
+const ceilingTrick = () => cardTrick(ceiling.png, 400);
+const sabethaTrick = () => cardTrick(sabetha.png, 400);
+const iciclesTrick = () => cardTrick(icicles.png, 400);
+let trickList = [elephantTrick, naplesTrick, bricksTrick, turtleTrick, creamTrick, bottleTrick, piggyTrick, guitarTrick, jasmineTrick, blanketTrick, ceilingTrick, sabethaTrick, iciclesTrick];
+for (let i=1; i<divList.length-2; i++){
+  divList[i].addEventListener('click', trickList[i-1]);
+}
 
 function cardSwapForward(target, duration) {
   let tl = anime.timeline({
@@ -231,9 +226,9 @@ function cardSwapBackward(target, duration) {
 
 function zReturn(){
   for (let i=0; i<length; i++){
-    pictureList[i].style.zIndex = '10';
+    divList[i].style.zIndex = '10';
     if(i !== 0){
-      pictureList[i].style.display = 'none';
+      divList[i].style.display = 'none';
     }
   }
 }
@@ -243,12 +238,12 @@ function cardSwapFull(button){
   //next arrow click
   if (button === nextArrow){
     if (current !== length-1){
-      pictureList[current+1].style.display = 'flex';
+      divList[current+1].style.display = 'flex';
     }else{
-      pictureList[0].style.display = 'flex';
+      divList[0].style.display = 'flex';
     }
 
-    cardSwapForward(pictureList[current], 400);
+    cardSwapForward(divList[current], 400);
     if (current === length-1){
       current = 0;
     }else {
@@ -259,13 +254,13 @@ function cardSwapFull(button){
   //prev arrow click
   if (button === prevArrow){
     if (current === 0){
-      pictureList[length-1].style.display = 'flex';
+      divList[length-1].style.display = 'flex';
       current = length;
-      cardSwapBackward(pictureList[current-1], 400);
+      cardSwapBackward(divList[current-1], 400);
       current--;
     }else {
-      pictureList[current-1].style.display = 'flex';
-      cardSwapBackward(pictureList[current-1], 400);
+      divList[current-1].style.display = 'flex';
+      cardSwapBackward(divList[current-1], 400);
       current--;
     }
   }
@@ -296,11 +291,7 @@ function dieRoll() {
   let tl = anime.timeline({
     targets: shuffle,
     easing: 'easeOutQuad',
-    duration: 1800
-  });
-  tl
-  .add({
-    rotate: 1080
+    duration: 3000
   });
   tl
   .add({
@@ -309,7 +300,7 @@ function dieRoll() {
   tl
   .add({
     rotate: 0
-  })
+  });
 }
 
 const next = () => cardSwapFull(nextArrow);
@@ -330,13 +321,13 @@ shuffle.addEventListener('click', dieAnimate);
 
 function displayChange(mode, exclude){
   if (mode === 'see'){
-    for (let i=0; i<pictureList.length; i++){
-      pictureList[i].style.display = 'flex';
+    for (let i=0; i<divList.length; i++){
+      divList[i].style.display = 'flex';
     }
   }else {
     for (let i=0; i<length; i++){
-      if (i !== pictureList.indexOf(exclude)){
-        pictureList[i].style.display = 'none';
+      if (i !== divList.indexOf(exclude)){
+        divList[i].style.display = 'none';
       }
     }
   }
@@ -387,14 +378,15 @@ function rng(min, max) {
 
 const see = () => displayChange('see');
 let random = rng(0, length)
-const hide = () => displayChange('hide', pictureList[random]);
-let firstCurrent = current;
+const hide = () => displayChange('hide', divList[random]);
 async function shuffleAnimation(){
+  let temp = current;
+  current = random;
   see();
-  setTimeout(hide, 5400);
+  setTimeout(hide, 6100);
   for (let i=0; i<3; i++){
     for (let i=0; i<pngList.length; i++){
-      if (i !== firstCurrent){
+      if (i !== temp){
         rotate(pngList[i], 'left', (rng(-200, 200)), rng(-400, 400), rng(-400, 400));
         await new Promise(r => setTimeout(r, 100));
       }
